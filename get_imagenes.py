e7d007abdc3ea11708f4f0c9a06e153827d067f8
Luis G. More <luigi362@gmail.com>
2017-07-12 18:12:00 -0400

    Modificacion para corregir invalid sintax

--- get_imagenes.py
+++ get_imagenes.py
@@ -10,7 +10,7 @@ def main():
 	nombreimagen='calhouse_0230.jpg'
 	i=0
 	for vx in ventx:
-		subprocess.call(['./imprimir-imagenes',str(ventx[i],str(venty[i]),str(cliplimit[i]])
+		subprocess.call(['./imprimir-imagenes',nombreimagen,str(ventx[i]),str(venty[i]),str(cliplimit[i])])
 		i+=1
 	# except Exception:
 	# 	print("error")

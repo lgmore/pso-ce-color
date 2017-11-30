8ee4e2873a8e66acd5975b5a129418782c228132
Luis G. Moré <lg_more@yahoo.com.ar>
2017-11-30 19:04:38 -0300

    se retoca el script consolidar.sh

--- implementacion-paper/consolidar.sh
+++ implementacion-paper/consolidar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat VAR* > vardecision.txt
-cat FUN* > objetivos.txt
+find . -type f -name 'FUN*' -exec cat {} + >> objetivos.txt
+find . -type f -name 'VAR*' -exec cat {} + >> vardecision.txt
 paste -d " " vardecision.txt objetivos.txt > resultados.txt
 

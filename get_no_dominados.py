6229138d9713683c9de480fca82631947fd0115b
Luis G. Moré <lg_more@yahoo.com.ar>
2017-12-06 23:20:21 -0300

    Nueva correccion en get_no_dominados
    se contemplan soluciones repetidas

--- get_no_dominados.py
+++ get_no_dominados.py
@@ -6,23 +6,34 @@ def nodominados():
 	
 	try:
 		formato = '%s %s %s %s %s %s %s %s %s'
-		indice, ventx, venty, cliplimit, entropia, ssimr, ssimg, ssimb = np.loadtxt('calhouse_237.csv', delimiter=' ', unpack=True)
+		indice, ventx, venty, cliplimit, entropia, ssimr, ssimg, ssimb = np.loadtxt('calhouse_230.csv', delimiter=' ', unpack=True)
 		listadominados=np.array([])
-		with open('calhouse_237'+'_2.csv', 'ab') as abc:
+		with open('calhouse_230'+'_2.csv', 'ab') as abc:
+
 			for idxeval, valeval in enumerate(ventx):
-				esdominado='N'
+				listadominados=np.append(listadominados,'N')
+
+			for idxeval, valeval in enumerate(ventx):
+				if listadominados[idxeval]=='S':
+					auxiliar=np.column_stack([indice[idxeval], ventx[idxeval],venty[idxeval],cliplimit[idxeval],entropia[idxeval],ssimr[idxeval],ssimg[idxeval],ssimb[idxeval],listadominados[idxeval]])
+					np.savetxt(abc, auxiliar ,fmt=formato, delimiter=" ")
+					continue
+				
 				for idx, val in enumerate(ssimr):
 					if (idxeval!=idx):
-						if 	ssimr[idxeval] <= ssimr[idx] and ssimg[idxeval] <= ssimg[idx] and ssimb[idxeval] <= ssimb[idx] and entropia[idxeval] <= entropia[idx]:
-								esdominado='S'
-								listadominados=np.append(listadominados,esdominado)
-								auxiliar=np.column_stack([indice[idxeval], ventx[idxeval],venty[idxeval],cliplimit[idxeval],entropia[idxeval],ssimr[idxeval],ssimg[idxeval],ssimb[idxeval],esdominado]) 
+						if ssimr[idxeval] == ssimr[idx] and ssimg[idxeval] == ssimg[idx] and ssimb[idxeval] == ssimb[idx] and entropia[idxeval] == entropia[idx]:
+							listadominados[idx]='S'
+							continue								
+						if ssimr[idxeval] <= ssimr[idx] and ssimg[idxeval] <= ssimg[idx] and ssimb[idxeval] <= ssimb[idx] and entropia[idxeval] <= entropia[idx]:
+								listadominados[idxeval]='S'
+								
+								auxiliar=np.column_stack([indice[idxeval], ventx[idxeval],venty[idxeval],cliplimit[idxeval],entropia[idxeval],ssimr[idxeval],ssimg[idxeval],ssimb[idxeval],listadominados[idxeval]]) 
 								print(idxeval,idx,auxiliar)
 								np.savetxt(abc, auxiliar ,fmt=formato, delimiter=" ")
 								break
-				if esdominado=='N':
-					listadominados=np.append(listadominados,esdominado)
-					auxiliar=np.column_stack([indice[idxeval], ventx[idxeval],venty[idxeval],cliplimit[idxeval],entropia[idxeval],ssimr[idxeval],ssimg[idxeval],ssimb[idxeval],esdominado]) 
+				if listadominados[idxeval]=='N':
+					# listadominados=np.append(listadominados,esdominado)
+					auxiliar=np.column_stack([indice[idxeval], ventx[idxeval],venty[idxeval],cliplimit[idxeval],entropia[idxeval],ssimr[idxeval],ssimg[idxeval],ssimb[idxeval],listadominados[idxeval]]) 
 					np.savetxt(abc,auxiliar ,fmt=formato, delimiter=" ")
 
 		print(listadominados)
